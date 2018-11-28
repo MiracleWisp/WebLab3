6f63fbfc21a0673554f8dca3afabb7574a530c0e
almdudler <lesha-serdyukov@mail.ru>
2018-11-28 20:53:01 +0300

    Stuff

--- src/main/java/main/Point.java
+++ src/main/java/main/Point.java
@@ -1,5 +1,8 @@
 package main;
 
+import static java.lang.StrictMath.pow;
+import static java.lang.StrictMath.sqrt;
+
 public class Point {
     private float x;
     private float y;
@@ -7,6 +10,13 @@ public class Point {
     private boolean success;
 
     Point(float x, float y, float r, boolean success) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.success = success;
+    }
+
+    Point(float x, float y, float r) {
         this.x = x;
         this.y = y;
         this.r = r;
@@ -26,5 +36,44 @@ public class Point {
 
     public boolean isSuccess() {
         return success;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    private boolean fits1() {
+        return
+                (this.x <= this.r/2) &&
+                        (this.x >= 0) &&
+                        (this.y <= this.r) &&
+                        (this.y >= 0);
+
+    }
+
+    private boolean fits2() {
+        if (this.y < 0 || this.x > 0 || this.x < -this.r/2 || this.y > this.r/2) return false;
+        return this.y <= sqrt(pow(this.r / 2, 2) - pow(this.x, 2));
+    }
+
+    private boolean fits3() {
+        if (this.x > 0 || this.y > 0) return false;
+        return this.y >= -this.x - this.r;
+    }
+
+    void checkArea() {
+        this.success = (this.fits2() || this.fits3() || this.fits1());
     }
 }
--- src/main/java/main/ResultsView.java
+++ src/main/java/main/ResultsView.java
@@ -2,11 +2,14 @@ package main;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-@ManagedBean(name="resultsTable")
+@ManagedBean(name = "resultsTable")
+@ViewScoped
 public class ResultsView implements Serializable {
 
     private List<Point> points;
@@ -14,7 +17,43 @@ public class ResultsView implements Serializable {
     @PostConstruct
     public void init() {
         points = new ArrayList<Point>();
-        points.add(new Point(4,5,6, true));
+        Point p = new Point(0.7f, 0.5f, 1);
+        p.checkArea();
+        points.add(p);
+    }
+
+    public void addPoint(float userX, float userY, float userR) {
+        Point point = new Point(userX, userY, userR);
+
+        int errCode;
+        float[] right_Rs = {1f, 2f, 3f, 4f, 5f};
+        float[] right_Xs = {-2.0f, -1.5f, -1.0f, -0.5f, 0.0f, 0.5f, 1.0f, 1.5f, 2.0f};
+
+        errCode = 1; // wrong R
+        for (float r : right_Rs) {
+            if (r == userR) {
+                errCode = 0;
+                break;
+            }
+        }
+
+        errCode = 2; // wrong X
+        for (float x : right_Xs) {
+            if (x == userX) {
+                errCode = 0;
+                break;
+            }
+        }
+
+        if ((userY < -5) || (userY > 3))
+            errCode = 3; // wrong Y
+
+        //Ну и тут что-то сделать с ними
+
+
+
+        point.checkArea();
+        points.add(point);
     }
 
     public List<Point> getPoints() {
