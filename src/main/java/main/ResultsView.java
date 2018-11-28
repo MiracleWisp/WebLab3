e11b012eed9dcbb10e85b3d1757d033827a746ab
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-11-28 22:31:22 +0300

    merge form and result view
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/main/java/main/ResultsView.java
+++ src/main/java/main/ResultsView.java
@@ -2,15 +2,44 @@ package main;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean(name = "resultsTable")
-@ViewScoped
+@SessionScoped
 public class ResultsView implements Serializable {
+
+    private float x;
+    private float y;
+    private float r;
+
+    public float getX() {
+        System.out.println("X=" + this.x);
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+        System.out.println("X=" + this.x);
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
 
     private List<Point> points;
 
@@ -22,38 +51,39 @@ public class ResultsView implements Serializable {
         points.add(p);
     }
 
-    public void addPoint(float userX, float userY, float userR) {
-        Point point = new Point(userX, userY, userR);
+    public void addPoint() {
+        Point point = new Point(x, y, r);
 
         int errCode;
         float[] right_Rs = {1f, 2f, 3f, 4f, 5f};
         float[] right_Xs = {-2.0f, -1.5f, -1.0f, -0.5f, 0.0f, 0.5f, 1.0f, 1.5f, 2.0f};
 
         errCode = 1; // wrong R
-        for (float r : right_Rs) {
-            if (r == userR) {
+        for (float _r : right_Rs) {
+            if (_r == r) {
                 errCode = 0;
                 break;
             }
         }
 
         errCode = 2; // wrong X
-        for (float x : right_Xs) {
-            if (x == userX) {
+        for (float _x : right_Xs) {
+            if (_x == x) {
                 errCode = 0;
                 break;
             }
         }
 
-        if ((userY < -5) || (userY > 3))
+        if ((y < -5) || (y > 3))
             errCode = 3; // wrong Y
 
         //Ну и тут что-то сделать с ними
 
 
-
         point.checkArea();
         points.add(point);
+        System.out.println(points.get(1).getX());
+        System.out.println(points.get(1).getR());
     }
 
     public List<Point> getPoints() {
